0530aabca052f6638844c10dfc3857fddc5d0019
Madison Parker <madison@parkerworld.com>
2021-05-12 13:26:29 -0400

    Iteration 1 of Emojional

--- EmojionalMiniProject/ViewController.swift
+++ EmojionalMiniProject/ViewController.swift
@@ -15,5 +15,18 @@ class ViewController: UIViewController {
     }
 
 
+@IBAction func showMessage(sender: UIButton) {
+    
+    // in as much detail as possible
+    // pseudo-code the things that need to happen
+    // when a user clicks an emoji button
+    
+    
+    let alertController = UIAlertController(title:"You feel...", message:"Yay!", preferredStyle:UIAlertController.Style.alert)
+    
+    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    
+    present(alertController, animated: true, completion: nil)
 }
 
+}
